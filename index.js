392ea96d1bc12ec7602df5b568bd217d94b2123b
Christian Kaisermann <christian@kaisermann.me>
2018-01-09 12:00:09 -0200

    Add submenu feature

--- index.js
+++ index.js
@@ -6,15 +6,45 @@ const waitForInput = require('./src/utils/waitForInput')
 
 binance.init()
 
+const menu = {
+  message: 'Menu',
+  choices: [{ name: 'My Account', value: 'submenu:account' }],
+  submenus: {
+    account: {
+      message: chalk.yellow('What you want to do?'),
+      choices: [{ name: 'See balance', value: 'balance' }]
+    }
+  }
+}
+
 const displayMenu = async () => {
-  const { action } = await inquirer.prompt([
-    {
+  let currentMenu = menu
+  let action = null
+  do {
+    currentMenu = {
       type: 'list',
       name: 'action',
-      message: chalk.yellow('What you want to do?'),
-      choices: [{ name: 'See balance', value: 'balance' }, 'Exit']
+      ...currentMenu
     }
-  ])
+    currentMenu.message = chalk.yellow(currentMenu.message)
+    const prompt = await inquirer.prompt(currentMenu)
+    action = prompt.action
+    if (action === 'go-back') {
+      currentMenu = currentMenu.parent
+      clear()
+    } else if (action.indexOf('submenu:') >= 0) {
+      const nextMenu = currentMenu.submenus[action.substring(8)]
+
+      nextMenu.parent = currentMenu
+      currentMenu = nextMenu
+
+      if (
+        currentMenu.choices[currentMenu.choices.length - 1].value !== 'go-back'
+      ) {
+        currentMenu.choices.push({ name: 'Go back', value: 'go-back' })
+      }
+    }
+  } while (action.indexOf('submenu:') >= 0 || action === 'go-back')
   switch (action.toLowerCase()) {
     case 'exit':
       return 0
