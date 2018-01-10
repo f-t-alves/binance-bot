a717ee25992164c50997db79ec8b732e2542d65b
Christian Kaisermann <christian@kaisermann.me>
2018-01-10 15:44:10 -0200

    Move balance render logic to balance route

--- src/binance.js
+++ src/binance.js
@@ -1,6 +1,4 @@
 const Binance = require('binance-api-node').default
-const chalk = require('chalk')
-const Table = require('cli-table2')
 const config = require('../config.json')
 const objectSum = require('./utils/objectSum.js')
 
@@ -38,14 +36,14 @@ module.exports = {
   destroy () {
     clearInterval(this.updateInterval)
   },
-  showBalances: async () => {
+  getBalances: async () => {
     if (prices === null) await updatePriceData()
 
     const accountInfo = await binance.accountInfo()
     const allBalances = accountInfo.balances
 
     let totals, smallBalances
-    const relevantBalances = allBalances
+    const balances = allBalances
       .reduce((acc, item) => {
         const symbol = item.asset
         const availableFunds = parseFloat(item.free)
@@ -81,47 +79,6 @@ module.exports = {
       // Sorting by number of coins (as binance do)
       .sort((a, b) => b.free - a.free)
 
-    const table = new Table({
-      head: [
-        'Symbol',
-        'Balance',
-        'In BTC',
-        'In ETH',
-        'In USD (btc)',
-        'In USD (eth)'
-      ]
-    })
-
-    for (const coin of relevantBalances) {
-      table.push([
-        chalk.green(coin.asset),
-        `${coin.free} ${coin.asset}`,
-        `${coin.valueIn.BTC.toFixed(8)} Ƀ`,
-        `${coin.valueIn.ETH.toFixed(8)} E`,
-        `${coin.valueIn.USDB.toFixed(2)} $`,
-        `${coin.valueIn.USDE.toFixed(2)} $`
-      ])
-    }
-    // If we have one or more coin balances below '0.09$',
-    // display their values together
-    if (smallBalances) {
-      table.push([
-        chalk.green('< 0.09$'),
-        '',
-        `${smallBalances.BTC.toFixed(8)} Ƀ`,
-        `${smallBalances.ETH.toFixed(8)} E`,
-        `${smallBalances.USDB.toFixed(2)} $`,
-        `${smallBalances.USDE.toFixed(2)} $`
-      ])
-    }
-    table.push([
-      chalk.yellow('Total'),
-      '',
-      `${totals.BTC.toFixed(8)} Ƀ`,
-      `${totals.ETH.toFixed(8)} E`,
-      `${totals.USDB.toFixed(2)} $`,
-      `${totals.USDE.toFixed(2)} $`
-    ])
-    console.log(table.toString())
+    return { balances, totals, smallBalances }
   }
 }
--- src/routes/account/balance.js
+++ src/routes/account/balance.js
@@ -1,7 +1,53 @@
 const binance = require('../../binance')
+const chalk = require('chalk')
+const Table = require('cli-table2')
+
+let accountBalances = {}
+
+const convertedValuesCells = obj => {
+  return [
+    `${obj.BTC.toFixed(8)} Ƀ`,
+    `${obj.ETH.toFixed(8)} E`,
+    `${obj.USDB.toFixed(2)} $`,
+    `${obj.USDE.toFixed(2)} $`
+  ]
+}
+
 module.exports = {
-  async onBeforeAppear () {},
+  async onBeforeAppear () {
+    accountBalances = await binance.getBalances()
+  },
   async render () {
-    await binance.showBalances()
+    const { balances, smallBalances, totals } = accountBalances
+
+    const table = new Table({
+      head: [
+        'Symbol',
+        'Balance',
+        'In BTC',
+        'In ETH',
+        'In USD (btc)',
+        'In USD (eth)'
+      ]
+    })
+
+    for (const coin of balances) {
+      table.push([
+        chalk.green(coin.asset),
+        `${coin.free} ${coin.asset}`,
+        ...convertedValuesCells(coin.valueIn)
+      ])
+    }
+    // If we have one or more coin balances below '0.09$',
+    // display their values together
+    if (smallBalances) {
+      table.push([
+        chalk.green('<0.09$'),
+        '',
+        ...convertedValuesCells(smallBalances)
+      ])
+    }
+    table.push([chalk.yellow('Total'), '', ...convertedValuesCells(totals)])
+    console.log(table.toString())
   }
 }
